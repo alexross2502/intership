41e048b7e65a8bf037814001a303d732b354f608
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-01-26 15:37:45 +0200

    .env.example added

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Suspense } from "react";
-import ClientPage from "./adminComponents/clientsPage/clientPage";
-import MainPage from "./mainPage";
-import MastersPage from "./adminComponents/mastersPage/mastersPage";
-import TownsPage from "./adminComponents/townsPage/townsPage";
-import AdminPage from "./adminComponents/adminPage/adminPage";
+import ClientPage from "./AdminComponents/ClientsPage/ClientPage";
+import MainPage from "./MainPage";
+import MastersPage from "./AdminComponents/MastersPage/MastersPage";
+import TownsPage from "./AdminComponents/TownsPage/TownsPage";
+import AdminPage from "./AdminComponents/AdminPage/AdminPage";
 
 function App() {
   return (
