8c19ded02bc3e63e2602062c1756f6168035bac0
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-02-02 05:22:47 +0200

    style: main AdminPage markup

--- web/src/AdminComponents/AdminPage/AdminPage.js
+++ web/src/AdminComponents/AdminPage/AdminPage.js
@@ -1,12 +1,19 @@
 import React from "react";
+import style from "./AdminPage.module.css";
 import { useTranslation } from "react-i18next";
+import ListSelector from "./Components/ListSelector";
 
 const AdminPage = () => {
   const { t } = useTranslation();
 
-  return <div>{t("adminPage.header")}</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.leftSide}>
+        <p className={style.leftSide__header}>{t("adminPage.header")}</p>
+      </div>
+      <div className={style.rightSide}></div>
+    </div>
+  );
 };
 
 export default AdminPage;
-
-//<div>{isAuthorized ? <Header /> : <Content />}</div>;
---
+++ web/src/AdminComponents/AdminPage/Components/ListSelector.js
@@ -0,0 +1,10 @@
+//import { useTranslation } from "react-i18next";
+import style from "./ListSelector.module.css";
+
+const ListSelector = () => {
+  //const { t } = useTranslation();
+
+  return <div className={style.container}>test text</div>;
+};
+
+export default ListSelector;
