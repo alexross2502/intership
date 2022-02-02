9ba69edfb2a004689e0b23424a8ee56beb94aded
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-02-02 19:37:16 +0200

    test: git crash

--- server/models/models.js
+++ server/models/models.js
@@ -3,6 +3,9 @@ const { Towns } = require("./Towns");
 const { Masters } = require("./Masters");
 const { Clients } = require("./Clients");
 
+Masters.hasOne(Towns);
+Towns.belongsTo(Masters);
+
 module.exports = {
   Admin,
   Towns,
