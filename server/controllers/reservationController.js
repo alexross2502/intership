bed551ecdabfd8e80a53b09a89e1f732a8c5b93f
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2022-06-13 16:33:29 +0300

    git test

--- server/controllers/reservationController.js
+++ server/controllers/reservationController.js
@@ -14,4 +14,21 @@ class ReservationController {
   }
 }
 
+async function create(day, start, end, master_id) {
+  try {
+    const { day, start, end, master_id } = req.body;
+    const reservation = await Reservation.create({
+      day,
+      start,
+      end,
+      master_id,
+    });
+    return res.json(reservation);
+  } catch (e) {
+    next(ApiError.badRequest(e.message));
+  }
+}
+
+//create("das", 13, 14);
+
 module.exports = new ReservationController();
